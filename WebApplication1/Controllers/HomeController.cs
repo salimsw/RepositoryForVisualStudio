db6b025837e865093da3d26887acbc8d2df0b53d
sshaikh <sshaikh@SALIMS>
2017-03-22 09:29:44 +0530

    change messa

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication1.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "This is first application with with git hub.";
 
             return View();
         }
